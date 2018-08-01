a6abe7858b3f951f04f4cc1999767606b65e641d
Reborn <2217379170@qq.com>
2018-08-01 13:58:58 +0800

    在SpringBootStudyExceptionHandler中新增判断ajax调用异常的处理方法。

--- src/main/java/zone/reborn/springbootstudy/handler/SpringBootStudyExceptionHandler.java
+++ src/main/java/zone/reborn/springbootstudy/handler/SpringBootStudyExceptionHandler.java
@@ -21,7 +21,11 @@ public class SpringBootStudyExceptionHandler {
 	public Object errorHandler(HttpServletRequest request, HttpServletResponse response, Exception e) {
 
 		e.printStackTrace();
-
+		
+		if(isAjax(request)) {
+			return "ajax"; //在正常开发中会进行返回结果的数据封装，这里只是简单的演示操作。
+		}
+		
 		ModelAndView mv = new ModelAndView();
 		mv.addObject("exception", e);
 		mv.addObject("url", request.getRequestURL());
@@ -29,6 +33,15 @@ public class SpringBootStudyExceptionHandler {
 		// mv.setViewName("/center/center.ftl");//不知道为什么，两个模板同时开启，始终加载不了freemarker的视图，等待考究
 		// mv.setViewName("/center/center.html");
 		return mv;
+	}
+	
+	/**
+	 * @param request
+	 * @description 判断是否是Ajax请求
+	 */
+	public static boolean isAjax(HttpServletRequest request) {
+		return (request.getHeader("X-Requested-With") != null
+				&& "XMLHttpRequest".equals(request.getHeader("X-Requested-With").toString()));
 	}
 
 }
